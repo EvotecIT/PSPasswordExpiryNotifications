67aff7c3498dbe6ef92e0924faa27e6cfed25de7
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-11-14 12:56:24 +0100

    1 char too much

--- Public/Start-PasswordExpiryCheck.ps1
+++ Public/Start-PasswordExpiryCheck.ps1
@@ -82,7 +82,7 @@ Function Start-PasswordExpiryCheck {
                         }
                         $u
                         if ($ConfigurationParameters.RemindersSendToUsers.SendCountMaximum -eq $Count) {
-                            Write-Color @WriteParameters -Text "[i] Sending email to maximum number of users ", "$($ConfigurationParameters.RemindersSendToUsers.SendCountMaximum) ", " has been reached. Skipping..." -Color White, Yellow, White
+                            Write-Color @WriteParameters -Text "[i] Sending email to maximum number of users ", "$($ConfigurationParameters.RemindersSendToUsers.SendCountMaximum) ", "has been reached. Skipping..." -Color White, Yellow, White
                             $TestingLimitReached = $true
                             break
                         }
