75e166243d833df0501348faa215c2fc95dcbb8b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-09-04 00:02:48 +0200

    Added Lowest / Highest in seperate functions

---
+++ Private/Get-LowestHighestDays.ps1
@@ -0,0 +1,25 @@
+﻿function Get-LowestHighestDays {
+    [cmdletBinding()]
+    param(
+        [System.Collections.IDictionary] $RemindersToUsers
+    )
+
+    $HighestLowest = Get-LowestHighestInternal -Rule $RemindersToUsers
+    foreach ($Rule in $RemindersToUsers.Rules) {
+        $PotentialHighestLowest = Get-LowestHighestInternal -Rule $Rule
+        if ($null -eq $PotentialHighestLowest.DayHighest) {
+            continue
+        }
+        if ($null -eq $HighestLowest.DayHighest) {
+            $HighestLowest = $PotentialHighestLowest
+        } else {
+            if ($PotentialHighestLowest.DayHighest -gt $HighestLowest.DayHighest) {
+                $HighestLowest.DayHighest = $PotentialHighestLowest.DayHighest
+            }
+            if ($PotentialHighestLowest.DayLowest -lt $HighestLowest.DayLowest) {
+                $HighestLowest.DayLowest = $PotentialHighestLowest.DayLowest
+            }
+        }
+    }
+    return $HighestLowest
+}
---
+++ Private/Get-LowestHighestInternal.ps1
@@ -0,0 +1,22 @@
+﻿function Get-LowestHighestInternal {
+    [cmdletBinding()]
+    param(
+        [System.Collections.IDictionary] $Rule
+    )
+    if ($Rule.Enable) {
+        if ($Rule.Reminders -is [System.Collections.IDictionary]) {
+            $DayHighest = Get-HashMaxValue -hashTable $Rule.Reminders
+            $DayLowest = Get-HashMaxValue -hashTable $Rule.Reminders -Lowest
+        } else {
+            [Array] $OrderedDays = $Rule.Reminders | Sort-Object -Unique
+            if ($OrderedDays.Count -gt 0) {
+                $DayHighest = $OrderedDays[-1]
+                $DayLowest = $OrderedDays[0]
+            }
+        }
+    }
+    [ordered] @{
+        DayHighest = $DayHighest
+        DayLowest  = $DayLowest
+    }
+}
