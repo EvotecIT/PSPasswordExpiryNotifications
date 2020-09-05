114d94e1055203b29667993d0cb95b32cfd28c9c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-09-05 19:30:20 +0200

    Examplee

---
+++ Example/ListUsers.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module .\PSPasswordExpiryNotifications.psd1 -Force
+Clear-Host
+#Find-PasswordExpiryCheck | Sort-Object -Property UserPrincipalName | Format-Table *
+
+
+Find-PasswordExpiryCheck | Where-Object { $_.PasswordNeverExpires -eq $true } | Sort-Object -Property PasswordLastSet | Format-Table *
