cae41d94ab635674adb9b334600ab9b8daef122c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-09-09 10:17:11 +0200

    Changed scope for LimitedScope

--- Private/Find-LimitedScope.ps1
+++ Private/Find-LimitedScope.ps1
@@ -10,16 +10,18 @@
             foreach ($Domain in $Forest.Domains) {
                 $Server = Get-ADDomainController -Discover -DomainName $Domain
                 try {
-                    $GroupMembers = Get-ADGroupMember -Identity $Group -Server $($Server.HostName) -ErrorAction Stop -Recursive
-                    #$GroupMembers
+                    #$GroupMembers = Get-ADGroupMember -Identity $Group -Server $($Server.HostName) -ErrorAction Stop -Recursive
+                    $GroupMembers = Get-ADGroup -Identity $Group -Server $($Server.HostName) -ErrorAction Stop -Properties Members
 
-                    foreach ($_ in $GroupMembers) {
-                        $CachedUsers["$($_.distinguishedName)"]
+                    #foreach ($_ in $GroupMembers) {
+                    #    $CachedUsers["$($_.distinguishedName)"]
+                    #}
+                    foreach ($_ in $GroupMembers.Members) {
+                        $CachedUsers["$($_)"]
                     }
-
                 } catch {
                     $ErrorMessage = $_.Exception.Message -replace "`n", " " -replace "`r", " "
-                    Write-Color @WriteParameters '[e] Error: ', $ErrorMessage -Color White, Red
+                    Write-Color @WriteParameters '[e] Managers Limited Scope Error: ', $ErrorMessage -Color White, Red
                     continue
                 }
             }
